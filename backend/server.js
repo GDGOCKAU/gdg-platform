2504e1df8c9ad93e14632b36abdb62f022c635d7
Saleh Bahakim <salehbahakimit@gmail.com>
2026-08-01 21:05:26 +0300

    server edit

--- backend/server.js
+++ backend/server.js
@@ -81,6 +81,7 @@ const adminOverviewRoutes = require("./routes/adminOverviewRoutes");
 const adminTeamRoutes = require("./routes/adminTeamRoutes");
 const adminSettingsRoutes = require("./routes/adminSettingsRoutes");
 const adminContestRoutes = require("./routes/adminContestRoutes");
+const adminProblemRoutes = require("./routes/adminProblemRoutes");
 // =======================================================
 // ===================== API Routes =======================
 // =======================================================
@@ -97,7 +98,7 @@ app.use("/api/admin/overview", adminOverviewRoutes);
 app.use("/api/admin/teams", adminTeamRoutes);
 app.use("/api/admin/settings", adminSettingsRoutes);
 app.use("/api/admin/contests", adminContestRoutes);
-
+app.use("/api/admin/problems", adminProblemRoutes);
 
 const PORT = process.env.PORT || 5000;
 
